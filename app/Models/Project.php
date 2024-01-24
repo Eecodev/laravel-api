32b6752dc8a8ecb0327c823a9809814212c03710
Eecodev <mnasser.bbk@gmail.com>
2024-01-24 17:16:23 +0100

    git update

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,7 +12,7 @@ class Project extends Model
 
         public function category()
         {
-            return $this->belongsTo(Category::class)->withTimestamps();
+            return $this->belongsTo(Category::class);
         }
 
         public function technologies()
